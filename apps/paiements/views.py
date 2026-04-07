2a24d9059d3fd5b49b51faaebe6043d663c18fff
Neila <neilakehila@gmail.com>
2026-04-07 16:33:29 +0100

    ajout de views.py de paiement

--- apps/paiements/views.py
+++ apps/paiements/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from .models import Paiement
+from .serializers import PaiementSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class PaiementListView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Paiement.objects.all()
+    serializer_class = PaiementSerializer
+    
+class PaiementDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Paiement.objects.all()
+    serializer_class = PaiementSerializer
