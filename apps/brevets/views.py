870788a73aa49a918509c8b4621e0a3145c14b83
Neila <neilakehila@gmail.com>
2026-04-07 16:38:52 +0100

    ajout de views dans brevet et notif

--- apps/brevets/views.py
+++ apps/brevets/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from .models import DemandeBrevet, Deposant, Inventeur, Brevet
+from .serializers import DemandeBrevetSerializer, DeposantSerializer, InventeurSerializer, BrevetSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class DemandeBrevetListView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = DemandeBrevet.objects.all()
+    serializer_class = DemandeBrevetSerializer
+    
+class DemandeBrevetDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = DemandeBrevet.objects.all()
+    serializer_class = DemandeBrevetSerializer  
+    
+class DeposantListView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Deposant.objects.all()
+    serializer_class = DeposantSerializer
+    
+class DeposantDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Deposant.objects.all()
+    serializer_class = DeposantSerializer
+    
+class InventeurListView(generics.ListCreateAPIView):  
+    permission_classes = [IsAuthenticated]
+    queryset = Inventeur.objects.all()
+    serializer_class = InventeurSerializer
+    
+class InventeurDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Inventeur.objects.all()
+    serializer_class = InventeurSerializer
+    
+class BrevetListView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Brevet.objects.all()
+    serializer_class = BrevetSerializer 
+    
+class BrevetDetailView(generics.RetrieveUpdateDestroyAPIView):  
+    permission_classes = [IsAuthenticated]
+    queryset = Brevet.objects.all()
+    serializer_class = BrevetSerializer
--- apps/notifications/views.py
+++ apps/notifications/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from .models import Notifications
+from .serializers import NotificationsSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class NotificationsListView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Notifications.objects.all()
+    serializer_class = NotificationsSerializer
+    
+class NotificationsDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Notifications.objects.all()
+    serializer_class = NotificationsSerializer
