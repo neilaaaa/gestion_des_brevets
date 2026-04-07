f9e690a6427fb619d95377bdf15606f0d4392c87
Neila <neilakehila@gmail.com>
2026-04-07 12:14:56 +0100

    ajouter views de recours.py

--- apps/recours/views.py
+++ apps/recours/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from .models import Recours
+from .serializers import RecoursSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+class RecoursListView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Recours.objects.all()
+    serializer_class = RecoursSerializer
+    
+    def get_queryset(self):
+        # Chaque user voit seulement ses propres recours
+        return Recours.objects.filter(id_user=self.request.user)
+    
+class RecoursDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Recours.objects.all()
+    serializer_class = RecoursSerializer
