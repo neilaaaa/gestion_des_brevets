8b0a3b08f93d0fa9c2b6a12d5a6d3804555704ce
Neila <neilakehila@gmail.com>
2026-04-07 01:51:03 +0100

    add dans view la fonction get_queryset

--- apps/documents/views.py
+++ apps/documents/views.py
@@ -2,19 +2,28 @@ from django.views.generic import ListView, DetailView
 from .models import Document, TypeDocument
 from .serializers import DocumentSerializer, TypeDocumentSerializer
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 class DocumentListView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Document.objects.all()
     serializer_class = DocumentSerializer
+    
+    def get_queryset(self):
+        # Chaque user voit seulement ses propres documents
+        return Document.objects.filter(id_user=self.request.user)
 
 class TypeDocumentListView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = TypeDocument.objects.all()
     serializer_class = TypeDocumentSerializer
 
 class DocumentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Document.objects.all()
     serializer_class = DocumentSerializer
     
 class TypeDocumentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = TypeDocument.objects.all()
     serializer_class = TypeDocumentSerializer
